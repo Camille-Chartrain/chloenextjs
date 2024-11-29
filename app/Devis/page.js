062f3ea3da5df7428f1ca55dded5feec3fab5972
Camille-Chartrain <camille.chartrain@oclock.school>
2024-11-29 19:41:01 +0100

    cleaning scss + form in progress

--- app/Devis/page.js
+++ app/Devis/page.js
@@ -7,95 +7,104 @@ const Devis = () => {
         <body>
             <Burger_logo />
             <main className="main">
-                <h2>Devis-Contact</h2>
+                <h2>Devis - Contact</h2>
                 <form method="POST">
-                    <label htmlFor="lastname">Nom </label>
-                    <input type="text" name="lastname" id="lastname" />
+                    <div className='segment'>
+                        <label htmlFor="lastname">Nom </label>
+                        <input type="text" name="lastname" id="lastname" />
 
 
-                    <label htmlFor="firstname">Prénom </label>
-                    <input type="text" name="firstname" id="firstname" />
+                        <label htmlFor="firstname">Prénom </label>
+                        <input type="text" name="firstname" id="firstname" />
 
 
-                    <label htmlFor="adresse">Adresse </label>
-                    <input type="text" name="adresse" id="adresse" />
+                        <label htmlFor="adresse">Adresse </label>
+                        <input type="text" name="adresse" id="adresse" />
 
-                    <label htmlFor="etage">Etage</label>
-                    <input type="text" name="etage" id="etage" />
-
-
-                    <span>Ascenseur:</span>
-
-                    <label htmlFor="oui">Oui </label>
-                    <input type="radio" name="ascenseur" id="oui" />
+                        <label htmlFor="etage">Etage</label>
+                        <input type="text" name="etage" id="etage" />
 
 
+                        <fieldset>
+                            <legend>Ascenseur :</legend>
+                            <div>
+                                <input type="radio" name="ascenseur" id="oui" />
+                                <label htmlFor="oui">Oui</label>
 
-                    <label htmlFor="non">Non</label>
-                    <input type="radio" name="ascenseur" id="non" />
-
-
-
-                    <label htmlFor="date">Date souhaitée du projet</label>
-                    <input type="text" name="date" id="date" />
-
-
-                    <label htmlFor="surface_mur">Surface des murs (hauteur x Largeur en mètres)</label>
-                    <input type="text" name="surface_mur" id="surface_mur" />
-
-
-                    <label htmlFor="surface_sol">Surface des sols (longueur x Largeur en mètres)</label>
-                    <input type="text" name="surface_sol" id="surface_sol" />
-
-                    <label htmlFor="surface_plafond">Surface des plafonds (longueur x Largeur en mètres)</label>
-                    <input type="text" name="surface_plafond" id="surface_plafond" />
-
-                    <label htmlFor="element">Précisez si vous avez des éléments à décorer (exemple: meuble vasque, niche, banquette, etc)</label>
-                    <input type="text" name="element" id="element" />
-
-
-
-                    <p>Type de support:</p>
-
-                    <div class="checkbox">
-                        <div class="checkbox-label ">
-
-                            <label htmlFor="carrelage">Carrelage
-                                {/* <!-- <input type="checkbox" name="support" id="carrelage" /> --> */}
-                            </label>
-
-                            <label htmlFor="placo">Placo
-                                {/* <!-- <input type="checkbox" name="support" id="placo" /> --> */}
-                            </label>
-
-                            <label htmlFor="mur pierre">Mur en pierre
-                                {/* <!-- <input type="checkbox" name="support" id="mur en pierre" /> --> */}
-                            </label>
-
-                            <label htmlFor="chape anhydrite">Chape anhydrite
-                                {/* <!-- <input type="checkbox" name="support" id="chape anhydrite" /> --> */}
-                            </label>
-
-                            <label htmlFor="chape ciment">Chape ciment
-
-                                {/* <!-- <input type="checkbox" name="support" id="chape ciment" /> --> */}
-                            </label>
-
-                            <label htmlFor="support">Autre
-                                {/* <!-- <input type="text" name="support" id="support"> --> */}
-                            </label>
-                        </div>
-
-                        <div class="box">
-                            <input type="checkbox" name="support" id="carrelage" />
-                            <input type="checkbox" name="support" id="placo" />
-                            <input type="checkbox" name="support" id="mur en pierre" />
-                            <input type="checkbox" name="support" id="chape anhydrite" />
-                            <input type="checkbox" name="support" id="chape ciment" />
-                            <input type="text" name="support" id="support" />
-                        </div>
+                                <input type="radio" name="ascenseur" id="non" />
+                                <label htmlFor="non">Non</label>
+                            </div>
+                        </fieldset>
                     </div>
-                    <button type="button" name="valider">Valider</button>
+
+                    <div className='segment'>
+                        <label htmlFor="date">Date souhaitée du projet</label>
+                        <input type="text" name="date" id="date" />
+
+
+                        <label htmlFor="surface_mur">Surface des murs (hauteur x Largeur en mètres)</label>
+                        <input type="text" name="surface_mur" id="surface_mur" />
+
+
+                        <label htmlFor="surface_sol">Surface des sols (longueur x Largeur en mètres)</label>
+                        <input type="text" name="surface_sol" id="surface_sol" />
+
+                        <label htmlFor="surface_plafond">Surface des plafonds (longueur x Largeur en mètres)</label>
+                        <input type="text" name="surface_plafond" id="surface_plafond" />
+
+                        <fieldset>
+                            <legend>Type de support:</legend>
+
+                            <div className="checkbox_choices">
+                                <div class="checkbox-label ">
+
+                                    <label htmlFor="carrelage">Carrelage
+                                        {/* <!-- <input type="checkbox" name="support" id="carrelage" /> --> */}
+                                    </label>
+
+                                    <label htmlFor="placo">Placo
+                                        {/* <!-- <input type="checkbox" name="support" id="placo" /> --> */}
+                                    </label>
+
+                                    <label htmlFor="mur pierre">Mur en pierre
+                                        {/* <!-- <input type="checkbox" name="support" id="mur en pierre" /> --> */}
+                                    </label>
+
+                                    <label htmlFor="chape anhydrite">Chape anhydrite
+                                        {/* <!-- <input type="checkbox" name="support" id="chape anhydrite" /> --> */}
+                                    </label>
+
+                                    <label htmlFor="chape ciment">Chape ciment
+
+                                        {/* <!-- <input type="checkbox" name="support" id="chape ciment" /> --> */}
+                                    </label>
+
+                                    <label htmlFor="support">Autre
+                                        {/* <!-- <input type="text" name="support" id="support"> --> */}
+                                    </label>
+                                </div>
+
+                                <div className="box">
+                                    <input type="checkbox" name="support" id="carrelage" />
+                                    <input type="checkbox" name="support" id="placo" />
+                                    <input type="checkbox" name="support" id="mur en pierre" />
+                                    <input type="checkbox" name="support" id="chape anhydrite" />
+                                    <input type="checkbox" name="support" id="chape ciment" />
+                                    <input type="text" name="support" id="support" />
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div className='segment'>
+                        <label htmlFor="element">Précisez si vous avez des éléments à décorer (exemple: meuble vasque, niche, banquette, etc)</label>
+                        <input type="text" name="element" id="element" />
+
+                        <label htmlFor="message">Message</label>
+                        <input type="text" name="message" id="message" />
+                    </div>
+
+                    <button type="button" name="valider">Envoyer ma demande de devis</button>
                 </form>
             </main>
         </body>
