e3e926e841a6cd3719a6d35859dc5a91677ff73d
Camille-Chartrain <camille.chartrain@oclock.school>
2024-12-12 15:45:34 +0100

    sending email is working, content on preparation

--- pages/api/send_mail.js
+++ pages/api/send_mail.js
@@ -23,17 +23,54 @@ export default async function handler(req, res) {
         try {
             console.log('dans le try');
 
+            const emailContent = `
+            <h1>Nouvelle demande de devis</h1>
+            <p><strong>Nom :</strong> ${otherData.lastname}</p>
+            <p><strong>Email :</strong> ${otherData.email}</p>
+            <p><strong>Téléphone :</strong> ${otherData.telephone}</p>
+            
+            <p><strong>Adresse :</strong> ${otherData.adresse}</p>
+            <p><strong>Etage :</strong> ${otherData.etage}</p>
+            <p><strong>Ascenseur : </strong> ${otherData.ascenseur}</p>
+            <p><strong>Parking : </strong> ${otherData.parking}</p>
+            <p><strong>Date souhaitée : </strong> ${otherData.date}</p>
+
+            <p><strong>Autre support mur:</strong> ${otherData.autre_support_murs} </p>
+            <p><strong>Autre support plafonds:</strong> ${otherData.autre_support_plafonds} </p>
+            <p><strong>Autre support sols:</strong> ${otherData.autre_support_sols} </p>
+
+            <p><strong>Surface murs : ${otherData.surface_mur}</strong></p>
+            <p><strong>Surface plafonds : ${otherData.surface_plafond}</strong></p>
+            <p><strong>Surface sols : ${otherData.surface_sol}</strong></p>
+
+            <p><strong>Message : ${otherData.message}</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+            <p><strong> :</strong></p>
+
+
+        `;
+
+
+
             await transporter.sendMail({
                 from: 'camille.chartrain@gmail.com',
                 to: otherData.email,
-                subject: 'Votre demande de devis',
-                text: "rien pour le moment",
+                subject: 'Demande de devis',
+                html: emailContent,
             });
             console.log("arrivés jusqu'après await transporter");
 
             res.status(200).json({ message: 'Email sent successfully' });
         } catch (error) {
-            res.status(500).json({ error: 'Failed to send email', details: error });
+            console.error('Erreur lors de l\'envoi de l\'email :', error);
+            res.status(500).json({ error: 'Failed to send email', details: error.message });
         }
     } else {
         res.setHeader('Allow', ['POST']);
