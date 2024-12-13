a61a4430589f7e8d0632017157c5442876757669
Camille-Chartrain <camille.chartrain@oclock.school>
2024-12-13 15:20:38 +0100

    email content done

--- pages/api/send_mail.js
+++ pages/api/send_mail.js
@@ -17,43 +17,65 @@ export default async function handler(req, res) {
         // console.log(otherData)
         // console.log("otherData.email", otherData.email)
         // console.log("req.body", req.body)
-        console.log('avant le try transporter send mail');
+        // console.log('avant le try transporter send mail');
 
+        function formatResults(object) {
+            return Object.entries(object)
+                .map(([key, value]) => `<p><strong>${key} :</strong> ${value.join(', ')}</p>`)
+                .join(''); // Convertir en une seule chaîne
+        }
+
+        const mursResults = formatResults(prestations.murs);
+        const solsResults = formatResults(prestations.sols);
+        const plafondsResults = formatResults(prestations.plafonds);
 
         try {
-            console.log('dans le try');
+            // console.log('dans le try');
 
             const emailContent = `
             <h1>Nouvelle demande de devis</h1>
+
+            <br>
+
             <p><strong>Nom :</strong> ${otherData.lastname}</p>
             <p><strong>Email :</strong> ${otherData.email}</p>
             <p><strong>Téléphone :</strong> ${otherData.telephone}</p>
-            
             <p><strong>Adresse :</strong> ${otherData.adresse}</p>
             <p><strong>Etage :</strong> ${otherData.etage}</p>
             <p><strong>Ascenseur : </strong> ${otherData.ascenseur}</p>
             <p><strong>Parking : </strong> ${otherData.parking}</p>
             <p><strong>Date souhaitée : </strong> ${otherData.date}</p>
 
+            <br>
+
+            <h2> MURS</h2>
+            <p><strong>Surface murs : ${otherData.surface_mur}</strong></p>
+            ${mursResults}
             <p><strong>Autre support mur:</strong> ${otherData.autre_support_murs} </p>
-            <p><strong>Autre support plafonds:</strong> ${otherData.autre_support_plafonds} </p>
+
+            <br>
+
+            <h2> SOLS</h2>   
+            <p><strong>Surface sols : ${otherData.surface_sol}</strong></p>      
+            ${solsResults}   
             <p><strong>Autre support sols:</strong> ${otherData.autre_support_sols} </p>
 
-            <p><strong>Surface murs : ${otherData.surface_mur}</strong></p>
+            <br>
+
+            <h2> PLAFONDS</h2>
             <p><strong>Surface plafonds : ${otherData.surface_plafond}</strong></p>
-            <p><strong>Surface sols : ${otherData.surface_sol}</strong></p>
+            ${plafondsResults}
+            <p><strong>Autre support plafonds:</strong> ${otherData.autre_support_plafonds} </p>
 
-            <p><strong>Message : ${otherData.message}</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
-            <p><strong> :</strong></p>
+            <br>
 
+            <h2> ELEMENTS</h2>
+            <p><strong>Element</strong> ${otherData.element} </p>
+
+            <br>
+
+            <h2>MESSAGE</h2>
+            <p> ${otherData.message}</p>
 
         `;
 
@@ -65,7 +87,7 @@ export default async function handler(req, res) {
                 subject: 'Demande de devis',
                 html: emailContent,
             });
-            console.log("arrivés jusqu'après await transporter");
+            // console.log("arrivés jusqu'après await transporter");
 
             res.status(200).json({ message: 'Email sent successfully' });
         } catch (error) {
