b8d4d382a2ec620b4f5a2f1e596d280a84b4c164
Camille-Chartrain <camille.chartrain@oclock.school>
2025-03-01 16:53:20 +0100

    balise changed for good architecture

--- app/page.js
+++ app/page.js
@@ -4,13 +4,15 @@ export default function Home() {
   return (
     <div className="message">
       <h1>Site en construction</h1>
-      <p>Retrouvez-moi sur <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> instagram
-      </a>
-        <div className="logo">
-          <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> <img src="/logo_instagram.png" alt="lien page instagram" class="icon_insta" />
-          </a>
-        </div>
+      <p>Retrouvez-moi sur
+        <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> instagram
+        </a>
       </p>
+      <p className="logo">
+        <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> <img src="/logo_instagram.png" alt="lien page instagram" class="icon_insta" />
+        </a>
+      </p>
+
     </div>
   );
 }
