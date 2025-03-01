2710056e3f5d5a1adbf31a6b21478b226b8705ed
Camille-Chartrain <camille.chartrain@oclock.school>
2025-03-01 16:37:55 +0100

    adding css to the message

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
-
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="message">
       <h1>Site en construction</h1>
+      <p>Retrouvez-moi sur <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> instagram
+      </a>
+        <div className="logo">
+          <a href="https://www.instagram.com/chloechartrain__/" target="_blank"> <img src="/logo_instagram.png" alt="lien page instagram" class="icon_insta" />
+          </a>
+        </div>
+      </p>
     </div>
   );
 }
