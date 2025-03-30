62eaa7222a0697ebfaaea83145ac79f11161ba71
Camille-Chartrain <camille.chartrain@oclock.school>
2025-03-30 15:39:05 +0200

    typo error corrected

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       </p>
       <p className="logo">
         <a href="https://www.instagram.com/chloechartrain__/" target="_blank">
-          <img src="/icons8-instagram(1).gif" alt="lien page instagram" class="icon_insta" />
+          <img src="/icons8-instagram(1).gif" alt="lien page instagram" className="icon_insta" />
         </a>
       </p>
 
