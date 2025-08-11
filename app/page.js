7711d86dcc3b802fbdbb3211c71be83906f338f1
Camille-Chartrain <camille.chartrain@oclock.school>
2025-08-11 17:35:20 +0200

    logo added in pudlib repo + intro text added

--- app/page.js
+++ app/page.js
@@ -15,6 +15,25 @@ export default function Home() {
         <div className="content">
           <BurgerMenu />
           {/* <h1>Texte au-dessus de la vidéo</h1> */}
+
+        </div>
+      </div>
+      <div className="intro">
+        <div>
+          A propos de nous :
+        </div>
+        <div>
+          Artisane spécialisée dans le béton ciré depuis plus de cinq ans, je mets mon expertise au service des projets les plus exigeants.
+          Formée en France et forte d’une culture du détail et d’un goût prononcé pour les matériaux nobles, je conçois chaque réalisation comme une pièce unique. Mon souhait est de penser pour durer et sublimer l’espace.
+        </div>
+        <div>
+          J’offre aux architectes, designers, hôteliers et particuliers un savoir-faire alliant rigueur, esthétique et passion du détail.
+        </div>
+        <div>
+          Parce que chaque espace mérite une finition qui lui est propre, je travaille aussi d’autres enduits décoratifs raffinés : enduits de chaux fines, marmorino, béton mural texturé… Ces revêtements sur mesure permettent d’explorer une palette plus large des sensations, tout en conservant une cohérence esthétique forte.
+        </div>
+        <div>
+          Nous ne travaillons pas simplement la matière, nous façonnons une atmosphère. Chaque projet est une quête d’excellence où texture, volume et lumière s’unissent pour créer des espaces brutes, singuliers et profondément habités.
         </div>
       </div>
     </div >
