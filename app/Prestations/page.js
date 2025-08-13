6c81ffbbcc7341af84f42cb4dbfe546125a4b114
Camille-Chartrain <camille.chartrain@oclock.school>
2025-08-13 14:15:37 +0200

    part durabilite added on page prestations

--- app/Prestations/page.js
+++ app/Prestations/page.js
@@ -43,6 +43,21 @@ const prestations = () => {
                     <h3>LES REVETEMENTS MURAUX</h3>
                 </Link>
             </div>
+            <p className='durabilite'>
+                <p className='title'>
+                    DURABILITE
+                </p>
+                <p className='text'>
+                    Le béton ciré est reconnu pour sa grande résistance et son élégance intemporelle. Cependant, il est essentiel de comprendre que le béton ciré et les enduits décoratifs sont des revêtements minces qui suivent fidèlement le support sur lequel ils sont appliqués.
+                </p>
+                <p className='text'>
+                    Nos maisons vivent : les structures se dilatent, la terre bouge et de légères microfissures peuvent apparaître. Ce phénomène n’est pas un défaut des matières mais le résultat naturel des mouvements du bâtiment et des supports.
+                </p>
+                <p className='text'>
+                    La prévention passe par une préparation rigoureuse du support et l’utilisation de produits de qualité. Néanmoins, envisager la possibilité de fissuration fait partie d’une approche réaliste et responsable. Un béton ciré bien posé et bien entretenu reste un revêtement durable, esthétique et unique qui évolue avec votre habitat.
+                </p>
+
+            </p>
         </>
     )
 }
