1f6b6a0e4e60b48c9a26bad6349cd6fae046a450
hugo LAMY <hugo.lamy@oskey.io>
2025-08-15 22:31:06 +0200

    multiple burger choices

--- app/Components/BurgerMenu/index.js
+++ app/Components/BurgerMenu/index.js
@@ -44,22 +44,97 @@ const BurgerMenu = () => {
           onMouseEnter={hideNav}
         ></div>
 
-        <div className="burgerIcon" onMouseEnter={showNav} onClick={toggleMenu}>
-          {/* Affiche les lignes de l'icône burger conditionnellement */}
-          {isOpen && (
-            <>
-              <div className="line line1"></div>
-              <div className="line line2"></div>
-              <div className="line line3"></div>
-            </>
-          )}
-          {!isOpen && (
-            <>
-              <div className="line"></div>
-              <div className="line"></div>
-              <div className="line"></div>
-            </>
-          )}
+        {
+          // 1 }
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line line3 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 2 }
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_2 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_2 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_2 line3 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 3
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_3 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_3 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_3 line3 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 3.2
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_32 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_32 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_32 line3 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 3.3
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_33 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_33 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_33 line3 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 4
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_4 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_4 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_4 line3 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_4 crox1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_4 crox2 ${isOpen ? "open" : ""}`}></div>
+        </div>
+
+        {
+          // 5
+        }
+        <div
+          className={`burgerIcon ${isOpen ? "open" : ""}`}
+          onMouseEnter={showNav}
+          onClick={toggleMenu}
+        >
+          <div className={`line_5 line1 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_5 line2 ${isOpen ? "open" : ""}`}></div>
+          <div className={`line_5 line3 ${isOpen ? "open" : ""}`}></div>
         </div>
 
         <h1 className="menuTitle" onMouseEnter={showNav}>
