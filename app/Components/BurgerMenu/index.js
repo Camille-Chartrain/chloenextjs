b0f67dd538d6d36eae01280f063df080be3f6a5d
hugo LAMY <hugo.lamy@oskey.io>
2025-08-15 14:52:05 +0200

    fix mouse in and out menu burger

--- app/Components/BurgerMenu/index.js
+++ app/Components/BurgerMenu/index.js
@@ -1,89 +1,80 @@
-"use client"
+"use client";
+
 import { useState } from "react";
-// import styles from "./BurgerMenu.module.css"; // Import du fichier CSS
-import Link from 'next/link';
-import "./style.scss"
+import Link from "next/link";
+import "./style.scss";
+
 const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen); // Inverse l'état du menu
-        console.log("isOpen", isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); // Inverse l'état du menu
+    console.log("isOpen", isOpen);
+  };
 
-    const handleMouseEnter = () => {
-        setIsOpen(true);
-    };
+  const showNav = () => {
+    setIsOpen(true);
+  };
 
-    const handleMouseLeave = () => {
-        setIsOpen(false);
-    };
+  const hideNav = () => {
+    setIsOpen(false);
+  };
 
-    return (
-        <div className="burgerMenu">
+  return (
+    <div className="burgerMenu">
+      <header className="burger_logo">
+        <nav className={`menu ${isOpen ? "open" : ""}`} onMouseEnter={showNav}>
+          <ul>
+            <li>
+              <Link href="/Prestations">TYPES DE PRESTATIONS</Link>
+            </li>
+            <li>
+              <Link href="/Realisations">RÉALISATIONS</Link>
+            </li>
+            <li>
+              <Link href="/Partenaires">PARTENAIRES</Link>
+            </li>
+            <li>
+              <Link href="/Devis">DEMANDE DE DEVIS</Link>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className={`menu_closer ${isOpen ? "open" : ""}`}
+          onMouseEnter={hideNav}
+        ></div>
 
-            {/* Menu déroulant */}
-            <nav className={`menu ${isOpen ? "open" : ""}`}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            >
+        <div className="burgerIcon" onMouseEnter={showNav} onClick={toggleMenu}>
+          {/* Affiche les lignes de l'icône burger conditionnellement */}
+          {isOpen && (
+            <>
+              <div className="line line1"></div>
+              <div className="line line2"></div>
+              <div className="line line3"></div>
+            </>
+          )}
+          {!isOpen && (
+            <>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </>
+          )}
+        </div>
 
-                <ul>
-                    <li><Link href="/Prestations">TYPES DE PRESTATIONS</Link></li>
-                    <li><Link href="/Realisations">RÉALISATIONS</Link></li>
-                    <li><Link href="/Partenaires">PARTENAIRES</Link></li>
-                    <li><Link href="/Devis">DEMANDE DE DEVIS</Link></li>
-                </ul>
-            </nav >
-
-            <header className="burger_logo"
-            // onMouseEnter={handleMouseEnter}
-            // onMouseLeave={handleMouseLeave}
-            >
-
-                <div
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    className="burgerIcon"
-                    onClick={toggleMenu}
-
-                >
-                    {/* Affiche les lignes de l'icône burger conditionnellement */}
-                    {isOpen && (
-                        <>
-                            <div className="line line1"></div>
-                            <div className="line line2"></div>
-                            <div className="line line3"></div>
-                        </>
-                    )}
-                    {!isOpen && (
-                        <>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                            <div className="line"></div>
-                        </>
-                    )}
-                </div>
-                <h1>
-                    <Link
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                        href="/"
-                        className='logo'
-                        aria-label="Retour à l'accueil">
-
-                        <img src="/cc_black.png"
-                            alt="logo chloé chartrain"
-                            className="img_CC">
-                        </img>
-
-                        CHLOÉ CHARTRAIN
-
-                    </Link>
-                </h1>
-            </ header>
-        </div >
-    );
+        <h1 className="menuTitle" onMouseEnter={showNav}>
+          <Link href="/" className="logo" aria-label="Retour à l'accueil">
+            <img
+              src="/cc_black.png"
+              alt="logo chloé chartrain"
+              className="img_CC"
+            ></img>
+            CHLOÉ CHARTRAIN
+          </Link>
+        </h1>
+      </header>
+    </div>
+  );
 };
 
 export default BurgerMenu;
