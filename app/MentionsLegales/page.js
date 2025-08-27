9da8c38a3451130bbc15fbadf011847f0323e707
Camille-Chartrain <camille.chartrain@oclock.school>
2025-08-27 16:52:17 +0200

    page mentions legales done

---
+++ app/MentionsLegales/page.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./style.scss";
+
+const MentionsLegales = () => {
+    return (
+        <section className="politics">
+            <h4>Mentions Légales</h4>
+
+            <p>En vigueur au : 26/08/2025</p>
+
+            <p>
+                Conformément aux dispositions des articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l'Économie Numérique (LCEN), il est précisé aux utilisateurs du site CHLOE CHARTRAIN l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.
+            </p>
+
+            <h5>1. Éditeur du site</h5>
+
+            <p>
+                <ul>
+                    <li>Chloé Chartrain</li>
+                    <li>Adresse : [Ton adresse postale]</li>
+                    <li>Email : connection.chloechartrain@gmail.com</li>
+                    <li>Téléphone : [Ton numéro de téléphone]</li>
+                    <li>Statut : Auto-entrepreneur</li>
+                    <li>SIRET : [Ton numéro SIRET]</li>
+                </ul>
+            </p>
+
+
+            <h5>2. Hébergeur</h5>
+
+            <p>Le site est hébergé par :
+                <ul>
+                    <li>Hostinger International Ltd</li>
+                    <li>Adresse : [adresse de l’hébergeur]</li>
+                    <li>Site web : https://www.hostinger.fr</li>
+                </ul>
+            </p>
+
+            <h5>3. Propriété intellectuelle</h5>
+
+            <p>
+                Le site ainsi que l’ensemble de son contenu (textes, images, graphismes, logo, icônes, etc.) sont protégés par le droit d’auteur.
+                Toute reproduction, représentation, modification ou exploitation, totale ou partielle, est interdite sans l’autorisation écrite préalable de CHLOE CHARTRAIN.
+            </p>
+
+            <h5>4. Responsabilité</h5>
+
+            <p>
+                L’éditeur du site met tout en œuvre pour assurer l’exactitude et la mise à jour des informations diffusées.
+                Toutefois, il ne peut être tenu responsable des erreurs, omissions ou résultats qui pourraient être obtenus par un mauvais usage de ces informations.
+            </p>
+
+            <h5>5. Données personnelles</h5>
+
+            <p>
+                Les informations recueillies via les formulaires de contact (nom, prénom, email, téléphone, adresse) sont utilisées uniquement dans le cadre du traitement de votre demande.
+                Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez d’un droit d’accès, de rectification et de suppression de vos données.
+                Pour l’exercer, contactez-nous à : connection.chloechartrain@gmail.com
+            </p>
+
+            <h5>6. Liens hypertextes</h5>
+
+            <p>
+                Le site peut contenir des liens vers d’autres sites internet.
+                Nous déclinons toute responsabilité concernant le contenu de ces sites tiers.
+            </p>
+
+
+        </section >
+    );
+};
+
+export default MentionsLegales;
